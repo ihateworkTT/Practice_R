8bcede3381519ef47883f5a8f58b65975f04a4b5
ihateworkTT <e10n910@gmail.com>
2019-09-26 17:24:33 +0900

    Update 20190923.R

--- 20190923.R
+++ 20190923.R
@@ -8,3 +8,20 @@ table(ans) / length(ans) #ratio
 #bar graph
 sum <- table(ans)
 barplot(sum, main="ANSWERS")
+
+#Case
+head(mtcars)
+carb <- mtcars[,"carb"]
+table(carb)
+barplot(table(carb), main="Carburetors", xlab="The numbera of carburetors", ylab="Frequency")
+
+##display multiple graphs in one window
+#make window
+par(mfrow=c(1,3))
+
+#plot 1
+barplot(table(mtcars$carb), main="the number of carburetors", col="blue")
+#plot 2
+barplot(table(mtcars$cyl), main="the number of cylender", col="green")
+#plot 3
+barplot(table(mtcars$gear), main="the number of gears", col="grey")
