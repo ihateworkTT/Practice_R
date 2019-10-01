8c289c386a56acc35d0f2ac58dffff72f55aa41d
ihateworkTT <e10n910@gmail.com>
2019-10-01 10:49:12 +0900

    Update 20191001.R

--- 20191001.R
+++ 20191001.R
@@ -1,37 +1,12 @@
 ## https://jjeongil.tistory.com/331?category=691385
-##scatter plot
 
-wt <- mtcars$wt
-mpg <- mtcars$mpg
+#Line graph
+month <- 1:12
+late <- sample(20,12,replace=T)
+plot(month, late, main="the number of Late students", type="l", lty=1, lwd=1, xlab="Month", ylab="count")
 
-plot(wt,mpg,main="car weight - mpg", xlab="car weight", ylab="miles per gallon (m/g)",pch=3, type="p")
-
-#multiple variables
-
-vars <- c("mpg", "disp", "drat", "wt")
-target <- mtcars[,vars]
-pairs(target,main="Multi Plots")
-
-#catter plot with group info.
-a <- iris[,3:4]
-point <- as.numeric(iris$Species)
-color <- c("red", "blue", "purple")
-plot(a, main="Iris Plot", pch=c(point), col=color[point])
-
-##Correlation
-beers <- sample(10,10,replace=T)
-bal <-sample(10,10,replace=T)*0.01
-tbl <- data.frame(cbind(beers,bal))
-
-#scatter plot
-plot(bal~beers, data=tbl)
-
-#regression model
-res=lm(bal~beers, data=tbl)
-#regression libe
-abline(res)
-#correlation analysis
-cor(beers,bal)
-
-#correlation among multiple variables
-cor(iris[,1:4])
+#Line graph by group
+late1 <- sample(20,12,replace=T)
+late2 <- sample(20,12,replace=T)
+plot(month, late1, main="the number of Late students", type="l", lty=2, lwd=1.5, col="blue")
+lines(month, late2, type="l", lty=3, col="green")
